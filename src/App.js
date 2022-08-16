4c278d5521f7b00ad45246e3ef4f48619ff4425e
Eduarda Souto <eduardasouto2823@gmail.com>
2022-08-16 10:49:54 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.png';
 import perfil from './perfil.png';
+import perfil2 from './perfil2.png';
+import perfil3 from './perfil3.png';
+import perfil4 from './perfil4.png';
+
 
 import './reset.css';
 import './index.css';
@@ -16,20 +20,23 @@ function App() {
 
         <h1>Quem está assistindo?</h1>
         <ul>
-
-
           <li>
             <img src={perfil} className='perfil' alt='perfil'></img>
+            <p>Duka</p>
+          </li>
+
+          <li>
+            <img src={perfil2} className='perfil' alt='perfil'></img>
             <p>Laura</p>
           </li>
 
           <li>
-            <img src={perfil} className='perfil' alt='perfil'></img>
+            <img src={perfil3} className='perfil' alt='perfil'></img>
             <p>Marcia</p>
           </li>
 
           <li>
-            <img src={perfil} className='perfil' alt='perfil'></img>
+            <img src={perfil4} className='perfil' alt='perfil'></img>
             <p>Marcelo</p>
           </li>
         </ul>
